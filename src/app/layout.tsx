b19a339bf1b66e6b50dd00d2fd45fde36780cc93
Raavi Narayana <114774469+Narayana212@users.noreply.github.com>
2023-12-29 13:11:21 +0530

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,13 @@
 import type { Metadata } from "next";
-import { Inter as FontSans } from "next/font/google";
+import { Inter  } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { ClerkProvider } from "@clerk/nextjs";
 import Navbar from "@/components/navbar";
 import { Toaster } from "@/components/ui/sonner";
 
-export const fontSans = FontSans({
+export const inter = Inter({
   subsets: ["latin"],
-  variable: "--font-sans",
 });
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,7 +25,7 @@ export default function RootLayout({
         <body
           className={cn(
             "min-h-screen bg-background font-sans grainy antialiased",
-            fontSans.variable
+            inter.className
           )}
         >
           <Navbar/>
